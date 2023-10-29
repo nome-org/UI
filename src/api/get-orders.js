cb135ed10b0949cee618cd90ee778a28f7f7b325
hz <13457698+hozzjss@users.noreply.github.com>
2023-10-29 12:54:55 +0200

    API integration - add html transaction to get orders

--- src/api/get-orders.js
+++ src/api/get-orders.js
@@ -1,4 +1,13 @@
 /**
+ * @typedef {Object} HTMLTransaction
+ * @property {number} id
+ * @property {string} created_at
+ * @property {string} updated_at
+ * @property {string} tx_id
+ * @property {"PENDING" | "CONFIRMED"} status
+ */
+
+/**
  * @typedef {Object} OrderWithStatus
  * @property {number} id
  * @property {string} created_at
@@ -6,6 +15,7 @@
  * @property {string | null} ordinals_bot_order_id
  * @property {string} receiver_address
  * @property {number | null} html_transaction_id
+ * @property {HTMLTransaction | null} html_transaction
  * @property {number | null} html_inscription_index
  * @property {"PENDING" | "CONFIRMED"} status
  */
