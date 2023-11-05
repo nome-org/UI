5ba3ff9b3b790a1cbe7ff7b6649e6113f270a9be
hz <13457698+hozzjss@users.noreply.github.com>
2023-11-05 18:23:36 +0200

    Remove unnecessary code in vue.config.js

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production'
-      //  ? '/inscribe': '/inscribe',
-      ? '/member': '/member',
-})
