f11bfa438549c3609d82c1e29661a43d60533e21
hz <13457698+hozzjss@users.noreply.github.com>
2023-11-05 18:31:37 +0200

    Update API URL in .env.example and api-instance.js

--- src/api/api-instance.js
+++ src/api/api-instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VUE_APP_API_URL,
+  baseURL: import.meta.env.VITE_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
