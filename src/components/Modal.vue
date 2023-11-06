8d23319cb983fcb5249051c882db8115a69157fb
hz <13457698+hozzjss@users.noreply.github.com>
2023-11-06 06:19:37 +0200

    Add Modal component for displaying content in a popup window

---
+++ src/components/Modal.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+defineProps({
+  isOpen: Boolean,
+});
+const emit = defineEmits(["onVisibilityChange"]);
+
+const close = (e: MouseEvent) => {
+  if (e.target !== e.currentTarget) return;
+  emit("onVisibilityChange", false);
+};
+</script>
+<template>
+  <div
+    v-if="isOpen"
+    @click="close"
+    style="overflow-y: scroll; z-index: 99"
+    class="position-fixed top-0 left-0 bg-black bg-opacity-80 w-screen h-screen flex items-center justify-center z-50"
+  >
+    <slot></slot>
+  </div>
+</template>
+<style scoped>
+.top-0 {
+  top: 0;
+}
+.left-0 {
+  left: 0;
+}
+.bg-black {
+  --tw-bg-opacity: 1;
+  background-color: rgb(0 0 0 / var(--tw-bg-opacity));
+}
+.bg-opacity-80 {
+  --tw-bg-opacity: 0.8;
+}
+.w-screen {
+  width: 100vw;
+}
+.h-screen {
+  height: 100vh;
+}
+
+.flex {
+  display: flex;
+}
+.items-center {
+  align-items: center;
+}
+.justify-center {
+  justify-content: center;
+}
+.z-50 {
+  z-index: 50;
+}
+</style>
