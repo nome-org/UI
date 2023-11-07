0709cb9ca90ae8b46167f9a3a1b89f7f8c20ac94
hz <13457698+hozzjss@users.noreply.github.com>
2023-11-07 02:57:11 +0200

    Fix Modal.vue class name for position-fixed

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -14,7 +14,7 @@ const close = (e: MouseEvent) => {
     v-if="isOpen"
     @click="close"
     style="overflow-y: scroll; z-index: 99"
-    class="position-fixed top-0 left-0 bg-black bg-opacity-80 w-screen h-screen flex items-center justify-center z-50"
+    class="fixed top-0 left-0 bg-black bg-opacity-80 w-screen h-screen flex items-center justify-center z-50"
   >
     <slot></slot>
   </div>
