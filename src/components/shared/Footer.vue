53940335e78fb16661250a34a9cedb6bd3ba75d4
hz <13457698+hozzjss@users.noreply.github.com>
2023-11-08 22:00:49 +0200

    Add footer component with social media links and website URL

---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="py-0 px-10 text-xl">
+    <div class="mt-12 pt-12">
+      <!--    <h1 class="intro-title">I AM NOME</h1>-->
+      <div
+        class="border-b border-solid border-opacity-20 border-white pt-2 md:mt-0 mt-12 w-full relative"
+      >
+        <div
+          class="absolute -top-[3.25rem] right-4 text-2xl lg:right-56 text-center"
+        >
+          <div
+            class="transition-all duration-75 hover:italic underline-offset-8 hover:underline"
+          >
+            <a
+              class="mx-0 mt-6 mb-20 text-white underline text-[1.4rem]"
+              target="_blank"
+              href="https://twitter.com/nome_nft"
+            >
+              Twitter</a
+            >
+          </div>
+
+          <div
+            class="transition-all duration-75 hover:italic underline-offset-8 hover:underline mt-3"
+          >
+            <a
+              class="mx-0 mt-6 mb-20 text-white underline text-[1.4rem]"
+              target="_blank"
+              href="https://discord.gg/ffZKc2TfN4"
+              >Discord</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full flex flex-col-reverse justify-between mt-6 pt-12 pb-10 gap-[1.8rem] sm:flex-row sm:gap-0"
+    >
+      <div class="text-lg">• it is not about me •</div>
+      <div class="">
+        <a class="mx-0 mt-6 mb-20" target="_blank" href="https://nome.wtf/"
+          ><div
+            class="text-white underline text-lg transition-all duration-75 hover:italic underline-offset-8 hover:underline"
+          >
+            nome.wtf
+          </div></a
+        >
+      </div>
+    </div>
+  </div>
+</template>
