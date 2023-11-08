5fb1d6fb1749e160573be305e9cda280cf99c2f4
hz <13457698+hozzjss@users.noreply.github.com>
2023-11-08 22:00:53 +0200

    Add SelectRarity component for selecting rarity in a game.

---
+++ src/components/shared/SelectRarity.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { available_rarity, rarityLabels } from "@/constants/rarity";
+
+const { selectedRarity } = defineProps({
+  selectedRarity: {
+    type: String,
+    required: true,
+  },
+});
+
+defineEmits(["update:selectedRarity"]);
+</script>
+<template>
+  <div class="flex flex-wrap sm:flex-row gap-8">
+    <div
+      v-for="rarity in available_rarity"
+      :key="rarity"
+      class="text-center basis-[45%]"
+    >
+      <button
+        @click="$emit('update:selectedRarity', rarity)"
+        :class="rarity == selectedRarity ? 'bg-gray-500' : 'bg-transparent'"
+        class="border-solid border border-white cursor-pointer uppercase h-10 w-full transition-all text-white rounded-xl"
+      >
+        {{ rarityLabels[rarity] }}
+      </button>
+    </div>
+  </div>
+</template>
