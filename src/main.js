83ec3daf03450def58fcfc2d4b2907e9e52cf7ac
hz <13457698+hozzjss@users.noreply.github.com>
2023-11-08 03:13:23 +0200

    Add Pinia state management and global styles.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-
 import VueGtag from "vue-gtag";
 import { VueQueryPlugin } from "@tanstack/vue-query";
+import { createPinia } from "pinia";
 
-const app = createApp(App)
+import App from "./App.vue";
+import "./global.css";
+import router from "./router";
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app
   .use(VueGtag, {
     config: { id: "G-HZQ4TKL9G9" },
   })
-  .use(VueQueryPlugin);
-app.mount("#app");
+  .use(pinia)
+  .use(router)
+  .use(VueQueryPlugin)
+  .mount("#app");
