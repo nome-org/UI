6dbac4ac10f51f6de783cee6e461a8fd8977ce32
hz <13457698+hozzjss@users.noreply.github.com>
2023-11-22 19:21:32 +0200

    Memories of the future is a great AOT episode

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-0 px-10 text-xl">
+  <div class="py-0 px-[25px] text-xl">
     <div class="mt-12 pt-12">
       <!--    <h1 class="intro-title">I AM NOME</h1>-->
       <div
@@ -36,25 +36,27 @@
     <div
       class="w-full flex justify-between mt-6 pt-14 pb-[48px] gap-0 relative"
     >
-      <div class="text-[20px]">• it is not about me •</div>
-      <div
-        class="text-white text-[15px] sm:text-[20px] pb-[25px] absolute bottom-[25px] left-[57%] md:left-1/2"
-      >
-        GIF-tool
-      </div>
+      <div class="text-[12px]">• it is not about me •</div>
+      <div>
+        <div
+          class="text-white text-[10px] sm:text-[20px] pb-[25px] absolute bottom-[25px] left-[57%] md:left-1/2"
+        >
+          GIF-tool
+        </div>
 
-      <a
-        href="/art"
-        class="text-white underline text-[15px] sm:text-[20px] transition-all duration-75 hover:italic underline-offset-8 hover:underline pb-[25px] absolute bottom-[25px] left-3/4"
-      >
-        Art
-      </a>
-      <a
-        href="https://iam.nome.wtf/about"
-        class="text-white underline text-[15px] sm:text-[20px] transition-all duration-75 hover:italic underline-offset-8 hover:underline pb-[25px] absolute bottom-[25px] right-0"
-      >
-        Artist →
-      </a>
+        <a
+          href="/art"
+          class="text-white underline text-[10px] sm:text-[20px] transition-all duration-75 hover:italic underline-offset-8 hover:underline pb-[25px] absolute bottom-[25px] left-3/4"
+        >
+          Art
+        </a>
+        <a
+          href="https://iam.nome.wtf/about"
+          class="text-white underline text-[10px] sm:text-[20px] transition-all duration-75 hover:italic underline-offset-8 hover:underline pb-[25px] absolute bottom-[25px] right-0"
+        >
+          Artist →
+        </a>
+      </div>
     </div>
   </div>
 </template>
