4a4a6db8cd1ec78e57e47bce012afd3463245215
hz <13457698+hozzjss@users.noreply.github.com>
2023-12-08 22:27:28 +0200

    add block9 rarity

--- src/constants/rarity.ts
+++ src/constants/rarity.ts
@@ -1,7 +1,9 @@
+// get rarities from ordinalsbot
 export const available_rarity = [
   "random",
   "pizza",
   "block78",
+  "block9",
   "vintage",
   "uncommon",
   "black",
@@ -11,6 +13,7 @@ export const rarityLabels = {
   random: "Random",
   pizza: "Pizza",
   block78: "Block 78",
+  block9: "Block 9",
   vintage: "Vintage",
   uncommon: "Uncommon",
   black: "Black",
